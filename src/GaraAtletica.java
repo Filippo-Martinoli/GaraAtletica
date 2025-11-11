aa5167494b3400400a18701ce52532e4f645802c
Gaia Mincigrcci <gaia.mincigrucci@gmail.com>
2025-11-11 14:27:47 +0100

    fix cartelle e aggiornamento codice

--- src/GaraAtletica.java
+++ src/GaraAtletica.java
@@ -1,27 +1,55 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.*;
+
 public class GaraAtletica {
-    public static void main(String[] args) {
-        System.out.println("Gara Atletica");
-        Atleta a1 = new Atleta(67, "Aureliano");
-        Atleta a2 = new Atleta(68, "Filippo");
-        Thread ta2 = new Thread(a2);
-        ta2.setPriority(Thread.MAX_PRIORITY);
-        Thread ta1 = new Thread(a1);
 
-        for (int i = 3; i > 0; i--) {
-            System.out.println("Inizio in:" + i);
+    public static void main (String[] args) {
+        int MIN_PART = 2;
+        int MAX_PART = 6;
+        int MIN_LUNGH = 10;
+        Scanner sc = new Scanner(System.in);
+        int nPartecipanti;
+        int lunghezzaPercorso;
+
+        System.out.println("Benvenuto alla gara atletica");
+
+        // Scegli numero partecipanti
+        do {
+            System.out.print("Numero partecipanti: ");
+            nPartecipanti = sc.nextInt();
+            if (nPartecipanti < MIN_PART || nPartecipanti > MAX_PART) {
+                System.out.printf("I partecipanti devono essere almeno %d e massimo %d\n", MIN_PART, MAX_PART);
+            }
+        } while(nPartecipanti < MIN_PART || nPartecipanti > MAX_PART);
+        sc.nextLine();
+
+        // Scegli lunghezza percorso
+        do {
+            System.out.printf("Lunghezza del percorso (min %d metri): ", MIN_LUNGH);
+            lunghezzaPercorso = sc.nextInt();
+            if(lunghezzaPercorso < MIN_LUNGH)
+                System.out.printf("La lunghezza del percorso deve essere di almeno %d metri", MIN_LUNGH);
+        } while(lunghezzaPercorso < MIN_LUNGH);
+        Atleta.lunghezzaPercorso = lunghezzaPercorso;
+
+        Giudice giudice = new Giudice(nPartecipanti);
+        List<Atleta> listaAtleti = new ArrayList<>();
+
+        for (int i = 0; i < nPartecipanti; i++) {
+            System.out.printf("Nome atleta %d: ", (i + 1));
+            String nome = sc.next();
+
+            System.out.printf("Numero atleta %d: ", (i + 1));
+            int numero = sc.nextInt();
+
+            listaAtleti.add(new Atleta(nome, numero, giudice));
         }
-        // System.out.println("Caratteristiche Main: "+ Thread.currentThread().getName()
-        // +" - " + Thread.currentThread().getPriority());
 
-        ta1.start();
-        try {
-            ta1.join();
-        } catch (InterruptedException e) {
-            System.err.println("Errore join");
+        giudice.dichiaraInizio();
+
+        for (int i = 0; i < listaAtleti.size(); i++) {
+            Thread t = new Thread(listaAtleti.get(i));
+            t.start();
         }
-
-        ta2.start();
+        sc.close();
     }
 }
